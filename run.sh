3643b8d090c900179089ebe0668647ee2d58a753
venra11 <fuentesj@willamette.edu>
2025-03-29 21:10:27 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
 psql $DATABASE_URL -f clean.sql
+anime_id=1
 
-for anime_id in {1..10}; do
+while true; do
+    # Get data and add to database if valid
     response=$(curl -s "https://api.jikan.moe/v4/anime/$anime_id")
-    echo "$response" | psql $DATABASE_URL -c "INSERT INTO anime_data (jsonb) VALUES ('\$1'::jsonb);" -f -
-    sleep 2
+    
+    if ! echo "$response" | grep -q "Not Found"; then
+        echo "Adding anime ID: $anime_id"
+        echo "$response" | psql $DATABASE_URL -c "INSERT INTO anime_data (jsonb) VALUES ('\$1'::jsonb);" -f -
+    fi
+    
+    # Move to next ID and wait
+    anime_id=$((anime_id + 1))
+    sleep 3
 done
+
