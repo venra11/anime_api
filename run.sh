2926436bc40a6f3186c6b64e11668b5ebe002834
venra11 <fuentesj@willamette.edu>
2025-03-29 20:59:46 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 psql $DATABASE_URL -f clean.sql
+
+for anime_id in {1..10}; do
+    response=$(curl -s "https://api.jikan.moe/v4/anime/$anime_id")
+    echo "$response" | psql $DATABASE_URL -c "INSERT INTO anime_data (jsonb) VALUES ('\$1'::jsonb);" -f -
+    sleep 2
+done
